26a15bad11ee2c5277ace052655026f5031c7e43
Dan Lorenc <dlorenc@google.com>
2021-07-11 10:32:15 -0500

    Initial scripts commit.

---
+++ scripts/update.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var url = "https://ftp.debian.org/debian/dists/bullseye/main/source/Sources.gz"
+
+func main() {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	gz, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(gz)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+
+	var w io.WriteCloser
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "Package: ") {
+			packageName := strings.TrimPrefix(line, "Package: ")
+			w, err = os.Create("packages/" + packageName)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(packageName)
+		}
+		if line == "" {
+			w.Close()
+		} else {
+			fmt.Fprintln(w, line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ scripts/update.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+go run scripts/update.go
